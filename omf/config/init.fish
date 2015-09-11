bab3bb4294b08a4d97865cb59ac85ea45d4baa5d
jeremiejig <me@jeremiejig.fr>
2015-09-11 22:55:32 +0200

    Adding omf config file

---
+++ omf/config/init.fish
@@ -0,0 +1,10 @@
+
+#bobthefish config option.
+#set -g theme_display_git no
+#set -g theme_display_git_untracked no
+#set -g theme_display_git_ahead_verbose yes
+#set -g theme_display_hg yes
+#set -g theme_display_virtualenv no
+set -g theme_display_ruby no
+#set -g theme_display_user yes
+set -g default_user paul
