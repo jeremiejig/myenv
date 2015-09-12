e86bbe5d0e390fa4b92d6d5e462dfaffa65cbc50
jeremiejig <me@jeremiejig.fr>
2015-09-12 15:27:18 +0200

    adding more common lib

--- lib/common.sh
+++ lib/common.sh
@@ -3,12 +3,49 @@
 # DOWNLOADER to a command
 # MYENV_PATH to the installation of MyEnv
 # 
+# function :
+# - die
+# - is_installed
+# - check_sudo
+# - confirm
+# 
 
 
 die () {
 	local status=$?
 	echo $@
 	exit $status
+}
+
+is_installed() {
+	type $1 > /dev/null 2>&1
+}
+
+confirm() {
+	echo -n "Do you want to run $*? [N/y] "
+	read REPLY < /dev/tty
+	if test "$REPLY" = "y" -o "$REPLY" = "Y"; then 
+		"$@" < /dev/tty
+	fi 
+}
+
+check_sudo () {
+	if test -n ${MYENV_HAS_SUDO+_}; then
+		if test ${MYENV_HAS_SUDO} = "1" ; then
+			return 0;
+		else
+			return 1;
+		fi
+	elif is_installed sudo; then
+		echo -n "Do you have admin right ? [N/y]"
+		read REPLY < /dev/tty
+		if test "$REPLY" = "y" -o "$REPLY" = "Y"; then 
+			MYENV_HAS_SUDO=1
+			return 0;
+		fi
+	else
+		return 1;
+	fi
 }
 
 init () {
